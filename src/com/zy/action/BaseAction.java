d1de5128e4a8aed2332e36f75612509457f5d335
zhangyong <rwerwrw>
2016-12-22 08:20:55 +0800

    tip

--- src/com/zy/action/BaseAction.java
+++
@@ -1,9 +0,0 @@
-package com.zy.action;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class BaseAction extends ActionSupport {
-
-	private static final long serialVersionUID = 5397766694413670847L;
-
-}
